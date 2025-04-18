f0c6d19898c561ea9e14c9741e60ed7fd99eaad6
VictorDev371 <victor.official2077@gmail.com>
2025-04-18 19:09:04 -0300

    dsjalkdjsa

--- 3. Projetos de aprendizagem/Calculadora_V1.py
+++ 3. Projetos de aprendizagem/Calculadora_V1.py
@@ -11,26 +11,28 @@ Escolha uma operação
  OBS: Digite apenas o número informado
       ''')
 quantidade = int(input('Quantidade totais de números: '))
-resultado = 0
-resultado_2 = 1
 operacao = int(input('Qual a operação matemática: '))
 
 if operacao in [1, 2, 3, 4]:
     for x in range(1, quantidade + 1):
         numero = float(input(f'{x}ª Número: '))
-        if operacao == 1:
-            resultado += numero
-        elif operacao == 2:
-            resultado = numero - resultado
-        elif operacao == 3:
-            resultado_2 *= numero
+        if x == 1:
+            resultado = numero
         else:
-            resultado = resultado / numero
+            if operacao == 1:
+                resultado += numero
+            elif operacao == 2:
+                resultado -= numero
+            elif operacao == 3:
+                resultado *= numero
+            else:
+                if numero == 0:
+                    print('Não é possivel dividir por zero')
+                    break
+                else:
+                    resultado /= numero
     print('Calculando...')
     time.sleep(2)
-    if operacao == 3:
-        print(f'O resultado foi {resultado_2}')
-    else:
-        print(f'O resultado foi {resultado}')
+    print(f'O resultado foi {resultado}')
 else:
     print('Você digitou algo errado, porfavor reinicie')
