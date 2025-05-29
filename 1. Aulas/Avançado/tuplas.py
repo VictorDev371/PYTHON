43690a20b5c54d262b874823633092cc7dcac6c6
VictorDev371 <victor.official2077@gmail.com>
2025-05-29 20:27:11 -0300

    atualization

---
+++ 1. Aulas/Avançado/tuplas.py
@@ -0,0 +1,6 @@
+lanche = ('sorvete', 'suco', 'pudim')
+for x in range(0, len(lanche)):
+    print(lanche[x])
+
+for x in lanche:
+    print(f'Eu vou comer {x}')
--- 2. Exercísios/ex070.py
+++ 2. Exercísios/ex070.py
@@ -1,47 +1,35 @@
-total_gasto = mais_de_mil = produto_barato = 0
-primeira_volta = True
+total_gasto = 0
+mais_de_mil = 0
+produto_barato = float('inf')
+nome_produto_barato = ''
 
 while True:
     print('-'*30)
 
-    if primeira_volta:
-        nome_produto = str(input('Qual o nome do produto?: ')).strip().capitalize()
-    
-        valor_produto = float(input('Qual o valor do produto?: '))
-        total_gasto += valor_produto
-        produto_barato += total_gasto
-        if valor_produto > 1000:
-            mais_de_mil += 1
-        
-        print('-'*30)
+    nome_produto = str(input('Qual o nome do produto?: ')).strip().capitalize()
+    valor_produto = float(input('Qual o valor do produto?: R$'))
 
-        resposta = str(input('Você quer continuar?: ')).strip().upper()
-        while resposta not in ['S', 'N']:
-            resposta = str(input('Você quer continuar?: ')).strip().upper()
+    total_gasto += valor_produto
 
-        if resposta == 'N':
-            print(f'O total gasto: R${total_gasto} \n {mais_de_mil} produtos custam mais de R$1000 \n O produto mais barato: {nome_produto_barato} custando R${produto_barato}')
-            break
-        primeira_volta = False
-    else:
-        nome_produto = str(input('Qual o nome do produto?: ')).strip().capitalize()
-    
-        valor_produto = float(input('Qual o valor do produto?: '))
-        total_gasto += valor_produto
-        if valor_produto > 1000:
-            mais_de_mil += 1
-        elif valor_produto < produto_barato:
-            produto_barato = valor_produto
-            nome_produto_barato = nome_produto
-        
-        print('-'*30)
+    if valor_produto > 1000:
+        mais_de_mil += 1
 
-        resposta = str(input('Você quer continuar?: ')).strip().upper()
-        while resposta not in ['S', 'N']:
-            resposta = str(input('Você quer continuar?: ')).strip().upper()
+    if valor_produto < produto_barato:
+        produto_barato = valor_produto
+        nome_produto_barato = nome_produto
 
-        if resposta == 'N':
-            print(f'O total gasto: R${total_gasto} \n {mais_de_mil} produtos custam mais de R$1000 \n O produto mais barato: {nome_produto_barato} custando R${produto_barato}')
-            break
-        primeira_volta = False
+    print('-'*30)
+
+    resposta = str(input('Você quer continuar? [S/N]: ')).strip().upper()
+    while resposta not in ['S', 'N']:
+        resposta = str(input('Resposta inválida. Digite S ou N: ')).strip().upper()
+
+    if resposta == 'N':
+        print(f'''
+Total gasto: R${total_gasto:.2f}
+{mais_de_mil} produtos custam mais de R$1000
+Produto mais barato: {nome_produto_barato} custando R${produto_barato:.2f}
+''')
+        break
+
     
