7091e50fcbffdf61416b076534962347989cf89c
VictorDev371 <victor.official2077@gmail.com>
2025-06-20 22:01:49 -0300

    sadasd

--- 2. Exercísios/ex072.py
+++ 2. Exercísios/ex072.py
@@ -1,9 +1,18 @@
 numeros = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte" )
-numeros2 = (range(21))
+
 usuario = int(input("Digite um número entre 0 e 20: "))
-
-while usuario not in numeros2:
+while usuario not in (range(21)):
      usuario = int(input("Tente novamente. Digite um número entre 0 e 20: "))
-
 print(f'O número foi de {usuario}, por extenso ficará {numeros[usuario]}')
 
+while True:
+     pergunta = str(input("Você quer continuar?[S/N]")).upper().lstrip()
+     if pergunta == "S":
+          usuario = int(input("Digite um número entre 0 e 20: "))
+          while usuario not in (range(21)):
+               usuario = int(input("Tente novamente. Digite um número entre 0 e 20: "))
+          print(f'O número foi de {usuario}, por extenso ficará {numeros[usuario]}') 
+     elif pergunta == "N":
+          break
+     else:
+          print('Porfavor tente novamente')
